ec2434b65208bc1b31a5886664a149e7a3ecdc11
Steve Huff <steve.huff@runkeeper.com>
2017-08-16 17:12:04 -0400

    first try at a tmpfs for file uploads

---
+++ manifests/tmpfs.pp
@@ -0,0 +1,32 @@
+# rk_tomcat::tmpfs
+#
+# Create a tmpfs filesystem for Tomcat to use as scratch space
+class rk_tomcat::tmpfs(
+  $group,
+  $mountpoint,
+  $ramdisk_size,
+) {
+
+  file { $mountpoint:
+    ensure => directory,
+    owner  => 'root',
+    group  => $group,
+    mode   => '0770',
+  } ->
+
+  mounttab { $mountpoint:
+    ensure  => present,
+    fstype  => 'tmpfs',
+    device  => 'tmpfs',
+    dump    => 0,
+    pass    => 2,
+    options => [
+      "size=${ramdisk_size}",
+    ]
+  } ->
+
+  mountpoint { $mountpoint:
+    ensure   => present,
+    remounts => true,
+  }
+}
