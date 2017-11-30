98ba03fa40f6cfac397ee222bff39fee5be8eb84
Tim Hartmann <tim.hartmann@runkeeper.com>
2017-11-30 11:16:00 -0500

    Adding consul agent to our builds

---
+++ manifests/consul.pp
@@ -0,0 +1,28 @@
+# Wrapper class for consul
+# == Class: rk_tomcat::consul
+#
+class rk_tomcat::consul (
+  $datacenter,
+  $tag_key,
+  $version,
+  ) {
+  class { '::consul':
+    version  => ${version},
+    service_enable => true,
+    service_ensure => false,
+    config_hash => {
+      'data_dir'             => '/opt/consul',
+      'datacenter'           => ${datacenter},
+      'log_level'            => 'INFO',
+      'retry_join'           => ["provider=aws", "tag_key=${tag_key}", "tag_value=true"],
+      'enable_script_checks' => true,
+      'ports'                => { 'dns' => 53 },
+      'raft_protocol'        => '3',
+    }
+  }
+
+  file { '/opt/consul/node-id':
+    ensure  => absent,
+    require => Class['consul']
+  }
+}
